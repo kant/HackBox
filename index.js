4d37ae4a53dc9670c57b5d8a03b027905d7d82f4
Henrik Joreteg <hjoreteg@gmail.com>
2015-10-23 15:30:54 -0700

    added: attempt to set up for azure deploy

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-// bootstrap server with babel require hook
-// so whole project can be written in ES6
-require("babel/register");
-require("./server");
+import Hapi from "hapi";
+import Good from "good";
+import GoodConsole from "good-console";
+import Vision from "vision";
+import Inert from "inert";
+import Lout from "lout";
+import Bell from "bell";
+import PaginationPlugin from "./plugins/paginate";
+import HackathonRoutes from "./routes/hackathons";
+import ProjectRoutes from "./routes/projects";
+import ParticipantRoutes from "./routes/participants";
+import UserRoutes from "./routes/users";
+import MemberRoutes from "./routes/members";
+import DataSetRoutes from "./routes/data-sets";
+import CommentRoutes from "./routes/comments";
+
+const server = new Hapi.Server({ debug: { request: ["error"] } });
+const port = process.env.PORT || 3000;
+
+server.connection({
+  host: "0.0.0.0",
+  routes: {
+    cors: {
+      credentials: true
+    }
+  },
+  port
+});
+
+server.register([
+  Inert,
+  Vision,
+  Lout,
+  Bell,
+  {
+    register: Good,
+    options: {
+      reporters: [{
+        reporter: GoodConsole,
+        events: { log: "*", response: "*" }
+      }]
+    }
+  },
+  HackathonRoutes,
+  ParticipantRoutes,
+  ProjectRoutes,
+  UserRoutes,
+  MemberRoutes,
+  CommentRoutes,
+  DataSetRoutes,
+  PaginationPlugin
+], (err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+server.start(() => {
+  process.stdout.write(`api server started at ${server.info.uri}\n`);
+});
+
+module.exports = server;
