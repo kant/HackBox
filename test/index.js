5a1a99d1e3062038f92ec61116547ee92820e36f
Zach Hale <zachhale@gmail.com>
2016-01-08 15:28:23 -0800

    Bring back tests!

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 require("babel/register");
 
-// require("./hackathons");
+require("./hackathons");
 require("./comments");
-// require("./members");
-// require("./participants");
-// require("./projects");
-// require("./project-search");
-// require("./users");
-// require("./user-search");
-// require("./hackathon-search");
+require("./members");
+require("./participants");
+require("./projects");
+require("./project-search");
+require("./users");
+require("./user-search");
+require("./hackathon-search");
 // require("./auth");
 
 // run this last to shut down the server
