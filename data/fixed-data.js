ab5bbfa4f1a459f0e2b59a2f4642ffb49fb8d66a
MSFT <msft@Soos-MacBook-Pro.local>
2017-05-03 14:27:21 -0700

    challenges exec

--- data/fixed-data.js
+++ data/fixed-data.js
@@ -183,20 +183,62 @@ const colorSchemes = [
 ];
 
 const executiveChallenges = [
-  "Judson Althoff",
-  "Chris Capossela",
-  "Jean-Philippe Courtois",
-  "Kurt DelBene",
-  "Scott Guthrie",
-  "Kathleen Hogan",
-  "Amy Hood",
-  "Rajesh Jha",
-  "Peggy Johnson",
-  "Terry Myerson",
-  "Kevin Scott",
-  "Harry Shum",
-  "Brad Smith",
-  "Jeff Weiner"
+  {
+    name: "Judson Althoff",
+    info: "Share ideas that enable the digital transformation of our customers."
+  },
+  {
+    name: "Chris Capossela",
+    info: "Yes, we need more fans – but suggest ways to take better care of our current fans."
+  },
+  {
+    name: "Jean-Philippe Courtois",
+    info: "Inspire us with your strategies to grow our business in Industry verticals."
+  },
+  {
+    name: "Kurt DelBene",
+    info: "What can we do to capitalize on gaming growth for PC, Mobile and Console?"
+  },
+  {
+    name: "Scott Guthrie",
+    info: "Show ways to make Azure the #1 cloud in commercial segments."
+  },
+  {
+    name: "Kathleen Hogan",
+    info: "How can we establish a culture of learning that is as strong as our culture of giving?"
+  },
+  {
+    name: "Amy Hood",
+    info: "Suggest ways we can link our internal product investments to customer-growth scenarios supported by data."
+  },
+  {
+    name: "Rajesh Jha",
+    info: "Build something to make students all around the world more productive."
+  },
+  {
+    name: "Peggy Johnson",
+    info: "Show me a disruptive innovation that will drive growth."
+  },
+  {
+    name: "Terry Myerson",
+    info: "Use Windows 10 Creators Update to make something that inspires the creator in all of us."
+  },
+  {
+    name: "Kevin Scott",
+    info: "How can we improve quality and speed by working better together on engineering assets?"
+  },
+  {
+    name: "Harry Shum",
+    info: "Build solutions that infuse AI into Microsoft products and services."
+  },
+  {
+    name: "Brad Smith",
+    info: "Demonstrate new ways for Microsoft talent and technology to help solve the world’s greatest societal problems."
+  },
+  {
+    name: "Jeff Weiner",
+    info: "Build something that demonstrates the power of connecting the professional cloud and our professional network."
+  }
 ];
 
 const motivationTypes = [
