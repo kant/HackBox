846962de80b68c27f487b7c2c75a3dce4468c6fe
Roman Gusiev <v-rogusi@microsoft.com>
2017-07-05 15:11:57 -0700

    Latest api’s update for checkin

--- routes/checkins.js
+++ routes/checkins.js
@@ -1,9 +1,6 @@
 import Boom from "boom";
 import Joi from "joi";
-import { updateUser, stringId, optionalId, countryArray,
-  projectArray, roleArray, newUser, paginationWithDeleted,
-  sortDirection } from "../data/validation";
-import db, { paginate } from "../db-connection";
+import db from "../db-connection";
 
 const register = function (server, options, next) {
 
@@ -17,51 +14,83 @@ const register = function (server, options, next) {
       tags: ["alias", "whitelist"],
       handler(request, reply) {
         const objectToReturn = {
+          userId: "",
+          name: "",
           registered: false,
           checkedIn: false,
           projects: []
         };
+        let userObject;
         const {alias, hackathonId} = request.params;
-        const response = db("users").where({alias: `${alias}@microsoft.com`}).then((result) => {
-          if (!result.length) { throw Boom.notFound(`User not found`); }
-          return db("participants").where({user_id: result[0].id, hackathon_id: hackathonId});
-        }).then((registeredUser) => {
-          if (!registeredUser.length) { reply(objectToReturn); }
 
+        db("users").where({alias: `${alias}@microsoft.com`}).then((result) => {
+          if (!result.length) {
+            return Promise.reject("User not found");
+          }
+          objectToReturn.userId = result[0].id;
+          objectToReturn.name = result[0].name;
+          return db("participants").where({
+            user_id: result[0].id,
+            hackathon_id: hackathonId
+          });
+        }).then((registeredUser) => {
+          if (!registeredUser.length) {
+            return Promise.reject({return: true, objectToReturn});
+          } else {
+            objectToReturn.registered = true;
+            userObject = registeredUser[0];
+            return db("checkins").where({user_id: userObject.user_id, hackathon_id: hackathonId});
+          }
+        })
+        .then((checkedInUser) => {
+          objectToReturn.checkedIn = !checkedInUser.length ? false : true;
+          return db("projects").select("projects.id", "projects.title", "projects.location").join("members", function () {
+            this.onIn("members.user_id", userObject.user_id)
+              .andOn("members.project_id", "=", "projects.id")
+              .onIn("projects.venue", "USA - Redmond, WA Hack Venue");
+          });
+        }).then((projects) => {
+            objectToReturn.projects = projects;
+            reply(objectToReturn);
+        }).catch((err) => {
+            if (err === "User not found") {
+                reply({msg: "User not found"});
+            } else if (err.return) {
+                reply(err.objectToReturn);
+            } else {
+                reply(Boom.create(500));
+            }
         });
-        
-        // reply(response);
+      },
+      validate: {
+        params: {
+          hackathonId: Joi.number().integer().positive().required(),
+          alias: Joi.string().min(1).max(30).trim().required()
+        }
       }
     }
   });
 
   server.route({
     method: "POST",
-    path: "/whitelist2",
+    path: "/checkin/hackathon/{hackathonId}/user/{userId}",
     config: {
-      description: "Add email to registered emails table",
-      tags: ["email", "whitelist"],
+      description: "Check in user for specific hackathon",
+      tags: ["checkin"],
       handler(request, reply) {
-
-        var response = db("whitelist").whereIn('email', request.payload.emails).then((result) => {
-          console.log(result.length);
-          if (result.length == 0) {
-            const emails = request.payload.emails.map((email) => {
-              return {email: email, organization_id: request.payload.organization_id};
-            })
-
-            return db("whitelist").insert(emails);
-          } else {
-            throw Boom.conflict(`Email is already in the invitation list`);
-          }
+        const {userId, hackathonId} = request.params;
+        
+        const response = db("checkins").insert({
+          user_id: userId,
+          hackathon_id: hackathonId
         });
 
-        reply(response)
+        reply(response);
       },
       validate: {
-        payload: {
-          emails: Joi.array().items(Joi.string().email().required()),
-          organization_id: Joi.number().required()
+        params: {
+          hackathonId: Joi.number().integer().positive().required(),
+          userId: Joi.string().min(1).max(140).trim().required()
         }
       }
     }
