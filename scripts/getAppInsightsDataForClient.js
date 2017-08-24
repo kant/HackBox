d712ea4e843daa22ecd047a99c1d327833783a55
Roman Gusiev <v-rogusi@microsoft.com>
2017-08-23 17:16:08 -0700

    Script for custom events updated

--- scripts/getAppInsightsDataForClient.js
+++ scripts/getAppInsightsDataForClient.js
@@ -4,63 +4,11 @@ var async = require('async');
 const fs = require('fs');
 
 
-
-
-// var allNames = require('./names');
-// var names = [];
-// var allEvents = [];
-// var counter = 0;
-// var wstream = fs.createWriteStream('events.txt');
-// var seriesToRun = [];
-// var aHundred = [];
-
-// var getBlobByName = function(name, callback) {
-//     blobSvc.getBlobToText('customevents', name, function(error, result, response) {
-//         if (!error) {
-//             wstream.write(result);
-//             console.log(counter);
-//             counter++;
-//             callback(null, 'Done');
-//         } else {
-//             callback(null, 'Error');
-//         }
-//     });
-// }
-
-// var runAll = function(array) {
-//     if (index > 0) {
-//         var aHundredToRun = [];
-//         for (var i = 0; i < 200; i++) {
-//             if (index > 0) {
-//                 aHundredToRun.push(array[index]);
-//                 index--;
-//             }
-//         }
-
-//         async.parallel(aHundredToRun, function(err, results) {
-//             console.log('====>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-//             runAll(seriesToRun);
-//         });
-//     } else {
-//         console.log('DONE DONE DONE');
-//         wstream.end();
-//     }
-// }
-
-// allNames.forEach((name) => {
-//     if (name.includes('/Event/2017-05') || name.includes('/Event/2017-06') || name.includes('/Event/2017-07') || name.includes('/Event/2017-08')) {
-//         seriesToRun.push(getBlobByName.bind(null, name));
-//     }
-// })
-
-// console.log(seriesToRun.length);
-// var index = seriesToRun.length - 1;
-
-// runAll(seriesToRun);
-
-
-
-
+var names = [];
+var allEvents = [];
+var counter = 0;
+var seriesToRun = [];
+var aHundred = [];
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++
@@ -93,6 +41,57 @@ const fs = require('fs');
 
 
 
+
+// var allNames = require('./names');
+// var wstream = fs.createWriteStream('events.txt');
+// var getBlobByName = function(name, callback) {
+//     blobSvc.getBlobToText('customevents', name, function(error, result, response) {
+//         if (!error) {
+//             wstream.write(result);
+//             console.log(counter);
+//             counter++;
+//             callback(null, 'Done');
+//         } else {
+//             callback(null, 'Error');
+//         }
+//     });
+// }
+
+// var runAll = function(array) {
+//     if (index > 0) {
+//         var aHundredToRun = [];
+//         for (var i = 0; i < 200; i++) {
+//             if (index > 0) {
+//                 aHundredToRun.push(array[index]);
+//                 index--;
+//             }
+//         }
+
+//         async.parallel(aHundredToRun, function(err, results) {
+//             runAll(seriesToRun);
+//         });
+//     } else {
+//         console.log('DONE DONE DONE');
+//         wstream.end();
+//     }
+// }
+
+// allNames.forEach((name) => {
+//     // if (name.includes('/Event/2017-05') || name.includes('/Event/2017-06') || name.includes('/Event/2017-07') || name.includes('/Event/2017-08')) {
+//     if (name.includes('/Event/2017-05') || name.includes('/Event/2017-06') || name.includes('/Event/2017-07') || name.includes('/Event/2017-08')) {
+//         seriesToRun.push(getBlobByName.bind(null, name));
+//     }
+// })
+
+// console.log(seriesToRun.length);
+// var index = seriesToRun.length - 1;
+
+// runAll(seriesToRun);
+
+
+
+
+
 //++++++++++++++++++++++++++++++++++
 //STREAM EVENTS FROM txt FILE TO CSV
 //++++++++++++++++++++++++++++++++++
@@ -108,11 +107,31 @@ const fs = require('fs');
 // lineReader.on('line', function (line) {
 //   var obj = JSON.parse(line);
   
-//   if (obj.event[0].name.includes('ProjectChange_')) {
+// //   if (obj.event[0].name.includes('ProjectChange_')) {
+// //       writer.write({name: obj.event[0].name, time: obj.context.data.eventTime, id: obj.context.custom.dimensions[0].id});
+// //       count++;
+// //       console.log(count);
+// //   }
+
+// //   if (obj.event[0].name.includes('edLogin')) {
+// //       writer.write({name: obj.event[0].name, time: obj.context.data.eventTime, id: obj.context.custom.dimensions[0].id});
+// //       count++;
+// //       console.log(count);
+// //   }
+
+// //   if (obj.event[0].name.includes('Action')) {
+// //       writer.write({name: obj.event[0].name, time: obj.context.data.eventTime, id: obj.context.custom.dimensions[0].id});
+// //       count++;
+// //       console.log(count);
+// //   }
+
+// if (obj.event[0].name.includes('PageView')) {
 //       writer.write({name: obj.event[0].name, time: obj.context.data.eventTime, id: obj.context.custom.dimensions[0].id});
 //       count++;
 //       console.log(count);
 //   }
+    
+    
   
 // });
 
