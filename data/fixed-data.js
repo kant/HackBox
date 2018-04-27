5e7cd735c87d058eda1afb7dd0f81e74cfa09111
v-vivasa <v-vivasa@microsoft.com>
2018-04-27 12:11:56 -0700

    Update challenges for create project

--- data/fixed-data.js
+++ data/fixed-data.js
@@ -241,22 +241,39 @@ const colorSchemes = [
 
 ];
 
+// const executiveChallenges = [
+
+//   "Judson Althoff--Empower customers to harness the power of the cloud to drive digital transformation.--Judson.Althoff@microsoft.com",
+//   "Chris Capossela--How do we grow millennial fans in both enterprise and consumer for Office?--Chris.Capossela@microsoft.com",
+//   "Jean-Philippe Courtois--Empower our field organization to identify the highest propensity Cloud customers and partners.--jeanc@microsoft.com",
+//   "Kurt DelBene--How can Microsoft empower employees and accelerate customer engagement with digital transformation?--Kurt.DelBene@microsoft.com",
+//   "Scott Guthrie--Show ways to make Azure the #1 cloud in commercial segments.--scottgu@microsoft.com",
+//   "Kathleen Hogan--How can we establish a culture of learning that is as strong as our culture of giving?--khogan@microsoft.com",
+//   "Amy Hood--Create examples of our internal first party systems being applicable, sellable showcases for our customers.--amyhood@microsoft.com",
+//   "Rajesh Jha--Show us how our products and services can work together seamlessly in service of end to end customer scenarios.--rajeshj@microsoft.com",
+//   "Terry Myerson--Use Windows 10 Creators Update to make something that inspires the creator in all of us.--Terry.Myerson@microsoft.com",
+//   "Kevin Scott--How can we help more developers innovate and achieve success with Microsoft technology and platforms?--kvnscott@microsoft.com",
+//   "Harry Shum--Build solutions that infuse AI into Microsoft products and services.--hshum@microsoft.com",
+//   "Brad Smith--Demonstrate new ways for Microsoft talent and technology to help solve the world’s greatest societal problems. (Hack for Good)--Brad.Smith@microsoft.com",
+//   "Jeff Weiner--How can Microsoft help grow LinkedIn faster while at the same time, differentiating Microsoft products to create more value for customers?--jweiner@linkedin.com"
+
+// ];
 const executiveChallenges = [
 
-  "Judson Althoff--Empower customers to harness the power of the cloud to drive digital transformation.--Judson.Althoff@microsoft.com",
-  "Chris Capossela--How do we grow millennial fans in both enterprise and consumer for Office?--Chris.Capossela@microsoft.com",
-  "Jean-Philippe Courtois--Empower our field organization to identify the highest propensity Cloud customers and partners.--jeanc@microsoft.com",
-  "Kurt DelBene--How can Microsoft empower employees and accelerate customer engagement with digital transformation?--Kurt.DelBene@microsoft.com",
-  "Scott Guthrie--Show ways to make Azure the #1 cloud in commercial segments.--scottgu@microsoft.com",
-  "Kathleen Hogan--How can we establish a culture of learning that is as strong as our culture of giving?--khogan@microsoft.com",
-  "Amy Hood--Create examples of our internal first party systems being applicable, sellable showcases for our customers.--amyhood@microsoft.com",
-  "Rajesh Jha--Show us how our products and services can work together seamlessly in service of end to end customer scenarios.--rajeshj@microsoft.com",
-  "Terry Myerson--Use Windows 10 Creators Update to make something that inspires the creator in all of us.--Terry.Myerson@microsoft.com",
-  "Kevin Scott--How can we help more developers innovate and achieve success with Microsoft technology and platforms?--kvnscott@microsoft.com",
-  "Harry Shum--Build solutions that infuse AI into Microsoft products and services.--hshum@microsoft.com",
-  "Brad Smith--Demonstrate new ways for Microsoft talent and technology to help solve the world’s greatest societal problems. (Hack for Good)--Brad.Smith@microsoft.com",
-  "Jeff Weiner--How can Microsoft help grow LinkedIn faster while at the same time, differentiating Microsoft products to create more value for customers?--jweiner@linkedin.com"
-
+  "Wild Growth Ideas--Use your own creativity and ideas to make something that will attract new customers and business growth.",
+  "Modern Life--Make something to address the unarticulated, unmet customer needs that make the work-to-life journey seamless and integrated.",
+  "Modern Workplace--Make something to unlock the creator in everyone and enable seamless teamwork not just in the workplace but also at school and at home across all the devices.",
+  "Business Apps--Make something that accelerates the digitization of every business process.",
+  "Applications and Infrastructure--Make something that enables customers to have consistent identity, developer extensibility and security across their application portfolio.",
+  "Data and AI--Make something that uses Data and AI to amplify human ingenuity and drives competitive advantage for businesses.",
+  "Gaming--Make something that enables gamers to play the games they want, with the people they want, on the devices they want.",  
+  "Microsoft Employees--Make something to empower Microsoft employees to achieve more.",
+  "Hack for Good--Make something that demonstrates how Microsoft talent and technology can help solve the world’s greatest societal problems.",
+  "AI for Earth--Make something with AI technology to amplify human ingenuity and advance sustainability around the globe.",
+  "Developers--Make something to help developers innovate with AI.",
+  "GSMO & WCB--Make something that accelerates our Sales culture of empowering digital success.",
+  "Marketing--Use your Marketing skills and ideas to make a marketing or business plan that helps us deliver on Microsoft Solution Areas.",
+  "Finance--Use your Finance skills and ideas to make something that helps teams factor in platform and infrastructure costs for new products and features."
 ];
 
 const projectMotivations = [
