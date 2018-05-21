3116c7e190689c48d15cf203ce5fc1532a2eced6
v-dakit <v-dakit@microsoft.com>
2018-05-21 12:57:56 -0700

    Development.json, production.json, replica.json, staging.json, test.json - set all to have max pool of 200 and min pool of 10. Also explicitly set aquireConnectionTimeout to the one minute. Oneweek.js - removed the misc console.log and reformatted some lines. Updated .gitignore with the intellij settings file ".idea".

--- routes/oneweek.js
+++ routes/oneweek.js
@@ -3,27 +3,18 @@ import * as _ from "lodash";
 import { onSuccess } from "./onSuccess";
 import { onError } from "./onError";
 
-
 const register = function (server, options, next) {
     server.route({
         method: "GET",
         path: "/oneweekstatus",
         config: {
-            auth: false,
-            description: "Get Oneweek hackathon ids that are current next and previous",
-            notes: [
-                `Will get the current hackathon.`
-            ].join(""),
+            description: "Get Oneweek hackathon ids that are - current, next, and previous",
+            notes: ["Will get the current hackathon oneweek settings."].join(""),
             tags: ["api", "list"],
             handler(request, resp) {
-                const {limit, offset} = request.query;
-
                 if (request.auth.credentials && request.auth.credentials.organization_id) {
                     request.query.organization_id = request.auth.credentials.organization_id;
                 }
-
-                console.log(getHackathonOneweek().toString());
-
                 getHackathonOneweek()
                     .then(_.partial(onSuccess, resp))
                     .catch(_.partial(onError, resp,
