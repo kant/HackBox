261bf56f9cd97a1216ccf100a879b8daef4e2cc0
v-vivasa <v-vivasa@microsoft.com>
2018-06-06 13:24:44 -0700

    Comment code change of test deploy

--- deploy.sh
+++ deploy.sh
@@ -81,10 +81,10 @@ selectNodeVersion () {
       NODE_EXE=node
     fi
     # Manually setting npm version to npm@3.10.10
-    if [[ -e "$PROGRAMFILES/npm/3.10.10/node_modules/npm/bin/npm-cli.js" ]]; then
+    #if [[ -e "$PROGRAMFILES/npm/3.10.10/node_modules/npm/bin/npm-cli.js" ]]; then
       NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.10.10\\node_modules\\npm\\bin\\npm-cli.js\""
-    else
-      NPM_CMD="\"$NODE_EXE\" \"D:\\Program Files (x86)\\npm\\5.6.0\\node_modules\\npm\\bin\\npm-cli.js\""
+    #else
+    #  NPM_CMD="\"$NODE_EXE\" \"D:\\Program Files (x86)\\npm\\5.6.0\\node_modules\\npm\\bin\\npm-cli.js\""
     fi
   else
     NPM_CMD=npm
